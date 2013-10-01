e5bf90eabbb909639cd04900abefff4a9ebc077c
Anton <antofik@gmail.com>
2013-10-01 16:47:39 +0400

    dummy => codeforces

---
+++ Codeforces/TaskB/Task.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codeforces.TaskB
+{
+    public class Task
+    {
+        public static void Main()
+        {
+            var task = new Task();
+            task.Solve();
+        }
+
+        void Solve()
+        {
+
+        }
+    }
+
+    #region Input
+
+    public class Input
+    {
+        private static string _line;
+
+        public static bool Next()
+        {
+            _line = Console.ReadLine();
+            return !string.IsNullOrEmpty(_line);
+        }
+
+        public static bool Next(out long a)
+        {
+            var ok = Next();
+            a = ok ? long.Parse(_line) : 0;
+            return ok;
+        }
+
+        public static bool Next(out long a, out long b)
+        {
+            var ok = Next();
+            if (ok)
+            {
+                var array = _line.Split(' ').Select(long.Parse).ToArray();
+                a = array[0];
+                b = array[1];
+            }
+            else
+            {
+                a = b = 0;
+            }
+
+            return ok;
+        }
+
+        public static bool Next(out long a, out long b, out long c)
+        {
+            var ok = Next();
+            if (ok)
+            {
+                var array = _line.Split(' ').Select(long.Parse).ToArray();
+                a = array[0];
+                b = array[1];
+                c = array[2];
+            }
+            else
+            {
+                a = b = c = 0;
+            }
+            return ok;
+        }
+
+        public static bool Next(out long a, out long b, out long c, out long d)
+        {
+            var ok = Next();
+            if (ok)
+            {
+                var array = _line.Split(' ').Select(long.Parse).ToArray();
+                a = array[0];
+                b = array[1];
+                c = array[2];
+                d = array[3];
+            }
+            else
+            {
+                a = b = c = d = 0;
+            }
+            return ok;
+        }
+
+        public static bool Next(out long a, out long b, out long c, out long d, out long e)
+        {
+            var ok = Next();
+            if (ok)
+            {
+                var array = _line.Split(' ').Select(long.Parse).ToArray();
+                a = array[0];
+                b = array[1];
+                c = array[2];
+                d = array[3];
+                e = array[4];
+            }
+            else
+            {
+                a = b = c = d = e = 0;
+            }
+            return ok;
+        }
+
+        public static List<long> Numbers()
+        {
+            Next();
+            return _line.Split(' ').Select(long.Parse).ToList();
+        }
+    }
+
+    #endregion
+
+}
