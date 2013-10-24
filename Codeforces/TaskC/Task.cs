c2af42dc19a53d113ee017fae1f71545ae7b4520
Anton <antofik@gmail.com>
2013-10-24 10:40:07 +0400

    c+

--- Codeforces/TaskC/Task.cs
+++ Codeforces/TaskC/Task.cs
@@ -15,7 +15,35 @@ namespace Codeforces.TaskC
 
         void Solve()
         {
+            long n, x;
+            Input.Next(out n, out x);
 
+            var numbers = Input.Numbers().OrderBy(c=>c).ToList();
+
+            var position = numbers.IndexOf(x);
+            if (position == -1) position = BinarySearch(numbers, x);
+
+            var answer = 0;
+            while (numbers[(numbers.Count - 1) / 2] != x)
+            {
+                numbers.Insert(position, x);
+                answer++;
+            }
+            Console.WriteLine(answer);
+        }
+
+        private int BinarySearch(IList<long> numbers, long x)
+        {
+            var l = 0;
+            var r = numbers.Count;
+            while (l < r)
+            {
+                var m = (l + r)/2;
+                if (numbers[m] == x) return m;
+                if (numbers[m] > x) r = m;
+                else l = m + 1;
+            }
+            return l;
         }
     }
 }
