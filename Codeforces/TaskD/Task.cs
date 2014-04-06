27ec6ac653bbacb633a4ee08d7f6f1e41fdecc0c
anton <antofik@gmail.com>
2014-04-06 17:48:01 +0400

    D+

--- Codeforces/TaskD/Task.cs
+++ Codeforces/TaskD/Task.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 /*Library*/
 
@@ -22,9 +24,43 @@ namespace Codeforces.TaskD
             }
         }
 
-        void Solve()
+        private void Solve()
         {
+            long n, l, k;
+            Input.Next(out n, out l, out k);
+            var p = Input.Numbers().Select(c => c/100d).ToList();
+            p.Insert(0, 0);
+            var a = Input.Numbers();
+            for (var i = 0; i < n; i++) a[i]++;
+            a.Insert(0, 0);
 
+            var d = new double[n + 1, n + 1, n + 1]; //tour prises bugs
+            d[0, 0, 0] = 1;
+
+            for (var tour = 1; tour <= n; tour++)
+            {
+                for (var prise = 0; prise < tour; prise++)
+                {
+                    for (var bug = 0; bug <= n; bug++)
+                    {
+                        //win
+                        d[tour, prise + 1, Math.Min(n, bug + a[tour])] += p[tour]*d[tour - 1, prise, bug];
+
+                        //lose
+                        d[tour, prise, bug] += (1 - p[tour])*d[tour - 1, prise, bug];
+                    }
+                }
+            }
+
+            var r = 0d;
+            for (var prise = l; prise <= n; prise++)
+            {
+                for (var bug = Math.Max(0, prise - k); bug <= n; bug++)
+                {
+                    r += d[n, prise, bug];
+                }
+            }
+            Console.WriteLine(r.ToString("F12", CultureInfo.InvariantCulture));
         }
     }
 }
