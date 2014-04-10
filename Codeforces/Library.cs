adf75427cfd0eccf7b6d08eaf299cd8bd49e37e1
anton <antofik@gmail.com>
2014-04-10 04:43:38 +0400

    red black tree

--- Codeforces/Library.cs
+++ Codeforces/Library.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 
@@ -342,5 +344,400 @@ namespace Codeforces
             get { return _data[i, j]; }
             set { _data[i, j] = value % Modulo; }
         }
+    }
+
+    public class RedBlackTree<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    {
+        private readonly List<Node> _items = new List<Node>();
+        private Node _root;
+        private readonly IComparer<TKey> _comparer;
+
+        public RedBlackTree()
+        {
+            _comparer = Comparer<TKey>.Default;
+        }
+
+        private Node _search(TKey key)
+        {
+            var node = _root;
+            while (node != null)
+            {
+                var i = _comparer.Compare(key, node.Key);
+                if (i == 0) return node;
+                node = i > 0 ? node.Right : node.Left;
+            }
+            return null;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            var node = new Node { Key = key, Value = value, Red = true };
+            _items.Add(node);
+            if (_root == null)
+            {
+                _root = node;
+                _root.Red = false;
+                return;
+            }
+            var x = _root;
+            Node y = null;
+            while (x != null)
+            {
+                y = x;
+                x = _comparer.Compare(key, x.Key) > 0 ? x.Right : x.Left;
+            }
+            node.Parent = y;
+            if (y == null) _root = node;
+            else if (_comparer.Compare(key, y.Key) > 0) y.Right = node;
+            else y.Left = node;
+            InsertFixup(node);
+        }
+
+        private void InsertFixup(Node node)
+        {
+            while (true)
+            {
+                var parent = node.Parent;
+                if (parent == null || !parent.Red || parent.Parent == null) break;
+                var grand = parent.Parent;
+
+                if (parent == grand.Left)
+                {
+                    var uncle = grand.Right;
+                    if (uncle != null && uncle.Red) /* parent and uncle both red: we could swap colors of them with grand */
+                    {
+                        parent.Red = false;
+                        uncle.Red = false;
+                        grand.Red = true;
+                        node = grand; /* and continue fixing tree for grand */
+                    }
+                    else if (node == parent.Right) /* we rotate around the parent */
+                    {
+                        node = parent;
+                        RotateLeft(node);
+                    }
+                    else /* rotate around grand and switch colors of grand and parent */
+                    {
+                        parent.Red = false;
+                        grand.Red = true;
+                        RotateRight(grand);
+                        if (grand == _root) _root = parent;
+                        break; /* and finish since tree is fixed */
+                    }
+                }
+                else
+                {
+                    if (_root.Parent != null) Debugger.Break();
+
+                    var uncle = grand.Left;
+                    if (uncle != null && uncle.Red) /* parent and uncle both red: we could swap colors of them with grand */
+                    {
+                        parent.Red = false;
+                        uncle.Red = false;
+                        grand.Red = true;
+                        node = grand; /* and continue fixing tree for grand */
+                    }
+                    else if (node == parent.Left) /* we rotate around the parent */
+                    {
+                        node = parent;
+                        RotateRight(node);
+                    }
+                    else /* rotate around grand and switch colors of grand and parent */
+                    {
+                        parent.Red = false;
+                        grand.Red = true;
+                        RotateLeft(grand);
+                        if (grand == _root) _root = parent;
+                        break; /* and finish since tree is fixed */
+                    }
+                }
+            }
+            _root.Red = false;
+        }
+
+        private void RotateLeft(Node node)
+        {
+            if (node.Right == null)
+                throw new NotSupportedException("Cannot rotate left: right node is missing");
+            var parent = node.Parent;
+            var right = node.Right;
+            right.Parent = parent;
+            if (parent != null)
+            {
+                if (parent.Left == node) parent.Left = right;
+                else parent.Right = right;
+            }
+            node.Right = right.Left;
+            if (node.Right != null)
+                node.Right.Parent = node;
+            right.Left = node;
+            node.Parent = right;
+        }
+
+        private void RotateRight(Node node)
+        {
+            if (node.Left == null)
+                throw new NotSupportedException("Cannot rotate right: left node is missing");
+            var parent = node.Parent;
+            var left = node.Left;
+            left.Parent = parent;
+            if (parent != null)
+            {
+                if (parent.Left == node) parent.Left = left;
+                else parent.Right = left;
+            }
+            node.Left = left.Right;
+            if (node.Left != null)
+                node.Left.Parent = node;
+            left.Right = node;
+            node.Parent = left;
+        }
+
+        public void Remove(TKey key)
+        {
+            var node = _search(key);
+            if (node == null) return;
+
+            var deleted = node.Left == null || node.Right == null ? node : Next(node);
+            var child = deleted.Left ?? deleted.Right;
+            if (child != null)
+                child.Parent = deleted.Parent;
+            if (deleted.Parent == null) /* root */
+                _root = child;
+            else if (deleted == deleted.Parent.Left)
+                deleted.Parent.Left = child;
+            else
+                deleted.Parent.Right = child;
+            if (node != deleted)
+            {
+                node.Key = deleted.Key;
+                node.Value = deleted.Value;
+            }
+
+            if (!deleted.Red)
+            {
+                DeleteFixup(child, deleted.Parent);
+            }
+        }
+
+        private void DeleteFixup(Node node, Node parent)
+        {
+            while (parent != null && (node == null || !node.Red))
+            {
+                if (node == parent.Left)
+                {
+                    var brother = parent.Right;
+                    if (brother.Red)
+                    {
+                        brother.Red = false;
+                        parent.Red = true;
+                        RotateLeft(parent);
+                        brother = parent.Right;
+                    }
+                    if ((brother.Left == null || !brother.Left.Red) && (brother.Right == null || !brother.Right.Red))
+                    {
+                        node = parent.Red ? _root : parent;
+                        brother.Red = true;
+                        parent.Red = false;
+                    }
+                    else
+                    {
+                        if (brother.Right == null || !brother.Right.Red)
+                        {
+                            if (brother.Left != null)
+                                brother.Left.Red = false;
+                            brother.Red = true;
+                            RotateRight(brother);
+                            brother = parent.Right;
+                        }
+                        if (!brother.Right.Red) Debugger.Break();
+                        brother.Red = parent.Red;
+                        parent.Red = false;
+                        if (brother.Right != null)
+                            brother.Right.Red = false;
+                        RotateLeft(parent);
+                        node = _root;
+                    }
+                }
+                else
+                {
+                    var brother = parent.Left;
+                    if (brother.Red)
+                    {
+                        brother.Red = false;
+                        parent.Red = true;
+                        RotateRight(parent);
+                        brother = parent.Left;
+                    }
+                    if ((brother.Right == null || !brother.Right.Red) && (brother.Left == null || !brother.Left.Red))
+                    {
+                        node = parent.Red ? _root : parent;
+                        brother.Red = true;
+                        parent.Red = false;
+                    }
+                    else
+                    {
+                        if (brother.Left == null || !brother.Left.Red)
+                        {
+                            if (brother.Right != null)
+                                brother.Right.Red = false;
+                            brother.Red = true;
+                            RotateLeft(brother);
+                            brother = parent.Left;
+                        }
+                        brother.Red = parent.Red;
+                        parent.Red = false;
+                        if (brother.Left != null)
+                            brother.Left.Red = false;
+                        RotateRight(parent);
+                        node = _root;
+                    }
+                }
+                parent = node.Parent;
+            }
+
+            node.Red = false;
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                var node = _search(key);
+                return node == null ? default(TValue) : node.Value;
+            }
+            set
+            {
+                var node = _search(key);
+                if (node == null) Add(key, value);
+                else node.Value = value;
+            }
+        }
+
+        public bool Check()
+        {
+            int count;
+            return _root == null || _root.Check(out count);
+        }
+
+        private Node Minimum(Node node)
+        {
+            var result = node;
+            while (result.Left != null) result = result.Left;
+            return result;
+        }
+
+        private Node Maximum(Node node)
+        {
+            var result = node;
+            while (result.Right != null) result = result.Right;
+            return result;
+        }
+
+        private Node Next(Node node)
+        {
+            Node result;
+            if (node.Right != null)
+            {
+                result = Minimum(node.Right);
+            }
+            else
+            {
+                while (node.Parent != null && node.Parent.Right == node)
+                    node = node.Parent;
+                result = node.Parent;
+            }
+            return result;
+        }
+
+        private Node Previous(Node node)
+        {
+            Node result;
+            if (node.Left != null)
+            {
+                result = Maximum(node.Left);
+            }
+            else
+            {
+                while (node.Parent != null && node.Parent.Left == node)
+                    node = node.Parent;
+                result = node.Parent;
+            }
+            return result;
+        }
+
+        public void Print()
+        {
+            var node = Minimum(_root);
+            while (node != null)
+            {
+                Console.Write(node.Red ? '*' : ' ');
+                Console.Write(node.Key);
+                Console.Write('\t');
+                node = Next(node);
+            }
+        }
+
+        #region Nested classes
+
+        [DebuggerDisplay("{ToString()}")]
+        private sealed class Node
+        {
+            public TKey Key;
+            public TValue Value;
+            public bool Red;
+
+            public Node Parent;
+            public Node Left;
+            public Node Right;
+
+            public bool Check(out int count)
+            {
+                count = 0;
+                if (Red)
+                {
+                    if (Left != null && Left.Red) return false;
+                    if (Right != null && Right.Red) return false;
+                }
+                var left = 0;
+                var right = 0;
+                if (Left != null && !Left.Check(out left)) return false;
+                if (Right != null && !Right.Check(out right)) return false;
+                count = left + (Red ? 0 : 1);
+                if (left != right)
+                {
+                    ;
+                }
+                return left == right;
+            }
+
+            public override string ToString()
+            {
+                return ToShortString(2);
+            }
+
+            private string ToShortString(int depth = 0)
+            {
+                return depth == 0
+                    ? string.Format("{0}{1}", Red ? "*" : "", Key)
+                    : string.Format("({1}<{0}>{2})", ToShortString(), Left == null ? "null" : Left.ToShortString(depth - 1), Right == null ? "null" : Right.ToShortString(depth - 1));
+            }
+        }
+
+        #endregion
+
+        #region IEnumerable
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
     }
 }
