e75bf431b669e74df341d612a59abac84bc8ef49
anton <antofik@gmail.com>
2014-04-14 01:09:17 +0400

    D+

--- Codeforces/TaskB/Task.cs
+++ Codeforces/TaskB/Task.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text;
 /*Library*/
 
 namespace Codeforces.TaskB
@@ -24,7 +26,46 @@ namespace Codeforces.TaskB
 
         void Solve()
         {
+            long n, k;
+            Input.Next(out n, out k);
+            var items = new List<Item>();
+            for (var i = 0; i < n; i++)
+            {
+                long c, t;
+                Input.Next(out c, out t);
+                items.Add(new Item{Index = i, Cost = c, Type = t});
+            }
 
+            items = items.OrderBy(c => c.Type).ThenByDescending(c => c.Cost).ToList();
+
+            var cost = 0d;
+            var builder = new StringBuilder(Environment.NewLine);
+            for (var i = 0; i < k - 1; i++)
+            {
+                builder.Append("1 ");
+                builder.Append(items[i].Index + 1);
+                builder.Append(Environment.NewLine);
+                cost += items[i].Cost/(items[i].Type==1 ? 2.0 : 1.0);
+            }
+            builder.Append(n - k + 1);
+            var minCost = double.PositiveInfinity;
+            for (var i = (int)k - 1; i < n; i++)
+            {
+                builder.Append(" ");
+                builder.Append(items[i].Index + 1);
+                cost += items[i].Cost;
+                if (items[i].Cost < minCost) minCost = items[i].Cost;
+            }
+            if (items[(int)k - 1].Type == 1) cost -= minCost / 2.0;
+            builder.Insert(0, cost.ToString("F1", CultureInfo.InvariantCulture));
+            Console.Write(builder);
+        }
+
+        class Item
+        {
+            public long Cost;
+            public long Type;
+            public int Index;
         }
     }
 }
--- Codeforces/TaskD/Task.cs
+++ Codeforces/TaskD/Task.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 /*Library*/
 
 namespace Codeforces.TaskD
@@ -12,7 +13,10 @@ namespace Codeforces.TaskD
             var task = new Task();
             try
             {
-                task.Solve();
+                unchecked
+                {
+                    task.Solve();
+                }
             }
             catch (Exception ex)
             {
@@ -24,7 +28,63 @@ namespace Codeforces.TaskD
 
         void Solve()
         {
+            long n, k;
+            Input.Next(out n, out k);
+            var V = new List<long>[n];
+            var parents = new Dictionary<long, long>();
 
+            for (var i = 0; i < n; i++)
+                V[i] = new List<long>();
+
+            for (var i = 0; i < n - 1; i++)
+            {
+                long a, b;
+                Input.Next(out a, out b);
+                V[--a].Add(--b);
+                V[b].Add(a);
+            }
+
+            var l = new List<long>((int)n+1);
+            /* relax tree to vertex=0*/
+            var stack = new Stack<long>((int) n);
+            stack.Push(0);
+            while (stack.Any())
+            {
+                var v = stack.Pop();
+                l.Add(v);
+                foreach (var u in V[v])
+                {
+                    V[u].Remove(v);
+                    parents[u] = v;
+                    stack.Push(u);
+                }
+            }
+
+            var one = new long[n + 1, k + 1];
+            for (var i = 0; i < n; i++)
+                one[i, 0] = 1;
+
+            stack.Clear();
+            stack.Push(0);
+            var result = 0L;
+            unchecked
+            {
+                l.Reverse();
+                foreach(var v in l)
+                {
+                    foreach (var u in V[v])
+                        for (var i = 1; i <= k; i++)
+                            one[v, i] += one[u, i - 1];
+
+                    long s = 0;
+                    foreach (var u in V[v])
+                        for (var i = 1; i < k; i++)
+                            s += one[u, i - 1]*(one[v, k - i] - one[u, k - i - 1]);
+                    result += one[v, k] + s/2;
+                }
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
