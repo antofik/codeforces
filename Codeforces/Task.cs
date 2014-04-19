3ff2057183c646f2eac820acf3e046d2a5444b34
anton <antofik@gmail.com>
2014-04-19 22:10:00 +0400

    cleared

--- Codeforces/Task.cs
+++ Codeforces/Task.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+
 /*Library*/
-using System.Collections.Generic;
 
 namespace Codeforces.Task
 {
     public class Task
     {
+        private static readonly Random Rand = new Random();
+
         public static void Main()
         {
             var task = new Task();
@@ -14,23 +16,7 @@ namespace Codeforces.Task
 
         void Solve()
         {
-            var tree = new RedBlackTree<int, Guid>();
-            var rand = new Random();
 
-            var l = new List<int>();
-            for (var i = 0; i < 10000; i++)
-            {
-                var key = rand.Next(0, 10000);
-                tree[key] = Guid.NewGuid();
-                if (rand.Next(4)==1) l.Add(key);
-            }
-
-            foreach (var key in l)
-            {
-                tree.Remove(key);
-            }
-
-           // tree.Print();
         }
     }
 }
