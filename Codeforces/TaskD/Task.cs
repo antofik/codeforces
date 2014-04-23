91644c2b2c28f7772d79f92d8582e259bba0be1c
anton <antofik@gmail.com>
2014-04-23 22:19:34 +0400

    E+

--- Codeforces/TaskD/Task.cs
+++ Codeforces/TaskD/Task.cs
@@ -25,6 +25,38 @@ namespace Codeforces.TaskD
 
         void Solve()
         {
+            int n;
+            Input.Next(out n);
+            var s = 1000000;
+            var X = new HashSet<int>(Input.ArrayInt());
+            var XX = new bool[s+1];
+            foreach (var x in X)
+                XX[x] = true;
+            var Y = new List<int>();
+
+            var j = 1;
+            foreach(var x in X)
+            {
+                if (!XX[x]) continue;
+                if (XX[s - x + 1])
+                {
+                    for(;j<=s/2;j++)
+                        if (!XX[j] && !XX[s - j + 1])
+                        {
+                            Y.Add(j);
+                            Y.Add(s - j + 1);
+                            XX[s - x + 1] = false;
+                            j++;
+                            break;
+                        }
+                }
+                else
+                {
+                    Y.Add(s - x + 1);
+                }
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(string.Join(" ", Y));
 
         }
     }
--- Codeforces/TaskE/Task.cs
+++ Codeforces/TaskE/Task.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 /*Library*/
 
@@ -23,9 +22,70 @@ namespace Codeforces.TaskE
             }
         }
 
+        private List<int>[] adjucent;
+        private HashSet<int>[] blocked;
+
+        int dfs(int v)
+        {
+            var adj = new List<int>();
+            foreach(var u in adjucent[v])
+                if (!blocked[v].Contains(u))
+                {
+                    adj.Add(u);
+                    blocked[v].Add(u);
+                    blocked[u].Add(v);
+                }
+
+            var unpaired = -1;
+            foreach (var u in adj)
+            {
+                var p = dfs(u);
+                if (p >= 0)
+                {
+                    Console.WriteLine("{0} {1} {2}", v + 1, u + 1, p + 1);
+                }
+                else
+                {
+                    if (unpaired == -1)
+                        unpaired = u;
+                    else
+                    {
+                        Console.WriteLine("{0} {1} {2}", u + 1, v + 1, unpaired + 1);
+                        unpaired = -1;
+                    }
+                }
+            }
+
+            return unpaired;
+        }
+
         void Solve()
         {
+            int n, m;
+            Input.Next(out n, out m);
+            adjucent = new List<int>[n];
+            blocked = new HashSet<int>[n];
+            for (var i = 0; i < n; i++)
+            {
+                adjucent[i] = new List<int>();
+                blocked[i] = new HashSet<int>();
+            }
 
+            for (var i = 0; i < m; i++)
+            {
+                int a, b;
+                Input.Next(out a, out b);
+                a--;
+                b--;
+                adjucent[a].Add(b);
+                adjucent[b].Add(a);
+            }
+
+            if (m%2==0)
+            {
+                dfs(0);
+            }
+            else Console.WriteLine("No solution");
         }
     }
 }
