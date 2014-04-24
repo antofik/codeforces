04e163f49c652359b47fe618b45496cab24aa246
Anton <antofik@gmail.com>
2014-04-24 18:01:22 +0400

    C

--- Codeforces/TaskC/Task.cs
+++ Codeforces/TaskC/Task.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
 /*Library*/
+using System.Text;
 
 namespace Codeforces.TaskC
 {
@@ -25,7 +25,31 @@ namespace Codeforces.TaskC
 
         void Solve()
         {
+            int t;
+            Input.Next(out t);
+            var output = new StringBuilder();
+            for(var i=0;i<t;i++)
+            {
+                int n, p;
+                Input.Next(out n, out p);
+                Make(n, p, output);
+            }
+            Console.WriteLine(output);
+        }
 
+        private void Make(int n, int p, StringBuilder output)
+        {
+            var g = new bool[n, n]; //graph
+            var k = 2*n + p; //total edges
+            var w = 2 + n/p; //edges from each vertex
+            var last = k - w*(n - 1); //edges from last vertex
+            for (var i = 0; i < n - 1; i++)
+            {
+                for (var j = 0; j < w; j++)
+                {
+                    g[i, i + j + 1] = g[i + j + 1, i] = true;
+                }
+            }
         }
     }
 }
