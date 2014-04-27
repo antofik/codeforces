6f738484f4b857dee9f98289fe0f37d31d72d966
anton <antofik@gmail.com>
2014-04-27 23:50:49 +0400

    ABCDE+

--- Codeforces/TaskD/Task.cs
+++ Codeforces/TaskD/Task.cs
@@ -11,21 +11,61 @@ namespace Codeforces.TaskD
         public static void Main()
         {
             var task = new Task();
-            try
+            task.Solve();
+        }
+
+        private HashSet<int> bad = new HashSet<int>();
+        private readonly Dictionary<long, int> cache = new Dictionary<long, int>();
+        private List<int> pr;
+        private int[] lp;
+
+        int Get(long value)
+        {
+            var result = 0;
+            if (value == 1) return result;
+            if (!cache.TryGetValue(value, out result))
             {
-                task.Solve();
+                var ok = false;
+                for(var i=0;pr[i]*pr[i] <= value;i++)
+                {
+                    if (value%pr[i] == 0)
+                    {
+                        ok = true;
+                        result = Get(value/pr[i]) + (bad.Contains(pr[i]) ? -1 : 1);
+                        break;
+                    }
+                }
+                if (!ok) result = bad.Contains((int)value) ? -1 : 1;
             }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(ex);
-                throw;
-            }
+            cache[value] = result;
+            return result;
         }
 
         void Solve()
         {
+            int N, M;
+            Input.Next(out N, out M);
+            var A = Input.ArrayLong().ToArray();
+            bad = new HashSet<int>(Input.ArrayInt());
 
+            Primes.SieveOfEratosthenes(1000000000, out pr);
+
+            var gcd = new long[N];
+            gcd[0] = A[0];
+            for (var i = 1; i < N; i++)
+                gcd[i] = Primes.Gcd(A[i], gcd[i - 1]);
+
+            var divisor = 1L;
+            for (var i = N - 1; i >= 0; i--)
+            {
+                var g = gcd[i]/divisor;
+                if (Get(g) < 0)
+                    divisor *= g;
+                A[i] /= divisor;
+            }
+            
+            var result = A.Sum(x => Get(x));
+            Console.WriteLine(result);
         }
     }
 }
--- Codeforces/TaskE/Task.cs
+++ Codeforces/TaskE/Task.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-
-/*Library*/
 
 namespace Codeforces.TaskE
 {
@@ -11,21 +8,51 @@ namespace Codeforces.TaskE
         public static void Main()
         {
             var task = new Task();
-            try
-            {
-                task.Solve();
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(ex);
-                throw;
-            }
+            task.Solve();
         }
 
-        void Solve()
-        {
+        private int N;
+        private bool[,] a;
 
+        private void dfs(HashSet<int> visited, int v)
+        {
+            visited.Add(v);
+            for (var i = 0; i < N; i++)
+                if (a[v, i] && i != v && !visited.Contains(i))
+                    dfs(visited, i);
+        }
+
+        private void dfs_inv(HashSet<int> visited, int v)
+        {
+            visited.Add(v);
+            for (var i = 0; i < N; i++)
+                if (a[i, v] && i != v && !visited.Contains(i))
+                    dfs_inv(visited, i);
+        }
+
+        private void Solve()
+        {
+            N = int.Parse(Console.ReadLine());
+            a = new bool[N, N];
+            for (var i = 0; i < N; i++)
+            {
+                var args = Console.ReadLine().Split();
+                for (var j = 0; j < N; j++)
+                    a[i, j] = args[j] != "0";
+            }
+
+            var s = 0;
+            while (!a[s, s]) s++;
+            var set = new HashSet<int>();
+            dfs(set, s);
+            var ok = set.Count == N;
+            if (ok)
+            {
+                set.Clear();
+                dfs_inv(set, s);
+                ok = set.Count == N;
+            }
+            Console.WriteLine(ok ? "YES" : "NO");
         }
     }
 }
