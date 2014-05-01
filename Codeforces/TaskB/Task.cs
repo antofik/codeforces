f599b6ef8ce9e2b2a40f9e1929cf5811f25b52b2
anton <antofik@gmail.com>
2014-05-01 11:46:07 +0400

    B+

--- Codeforces/TaskB/Task.cs
+++ Codeforces/TaskB/Task.cs
@@ -16,7 +16,24 @@ namespace Codeforces.TaskB
 
         void Solve()
         {
-
+            var n = int.Parse(Console.ReadLine().Split()[0]);
+            var set = new HashSet<int>();
+            for (var t = 0; t < n; t++)
+            {
+                var line = Console.ReadLine();
+                var gnome = line.IndexOf('G');
+                var candy = line.IndexOf('S');
+                if (gnome > candy)
+                {
+                    Console.WriteLine(-1);
+                    return;
+                }
+                if (gnome < candy)
+                {
+                    set.Add(candy - gnome);
+                }
+            }
+            Console.WriteLine(set.Count);
         }
     }
 }
