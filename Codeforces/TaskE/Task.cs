e9675394b09482832bab7da9ef2c09b90101a925
anton <antofik@gmail.com>
2014-05-04 00:29:37 +0400

    E+

--- Codeforces/TaskE/Task.cs
+++ Codeforces/TaskE/Task.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-
-/*Library*/
+using System.Globalization;
+using System.Text;
 
 namespace Codeforces.TaskE
 {
@@ -16,7 +14,29 @@ namespace Codeforces.TaskE
 
         void Solve()
         {
-
+            var n = int.Parse(Console.ReadLine());
+            var output = new StringBuilder();
+            if (n == 5)
+            {
+                output.AppendLine("1 2 3");
+                output.AppendLine("1 3 3");
+                output.AppendLine("2 4 2");
+                output.AppendLine("4 5 1");
+                output.AppendLine("3 4");
+                output.AppendLine("3 5");
+            }
+            else
+            {
+                var n_ = n/2;
+                for (var i = 1; i <= n_; i++)
+                    output.AppendLine(string.Format("{0} {1} 1", i, i + n_));
+                for (var i = n_+1; i < n; i++)
+                    output.AppendLine(string.Format("{0} {1} {2}", i, i + 1, 2*(i - n_) - 1));
+                output.AppendLine("1 3");
+                for (var i = 1; i < n_; i++)
+                    output.AppendLine(string.Format("{0} {1}", i, i + 1));
+            }
+            Console.Write(output);
         }
     }
 }
