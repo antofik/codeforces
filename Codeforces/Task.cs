ae3a8e38127208b431b4c4eb30b24c473a8b5bba
antofik <antofik@gmail.com>
2015-09-08 13:03:15 +0300

    ABCDE

--- Codeforces/Task.cs
+++ Codeforces/Task.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,9 +7,73 @@ namespace Codeforces.Task/*#*/
 {
     public class Task
     {
+        private List<int>[] tree;
+        private byte[] colors;
+        private int[] back;
+        const int WHITE = 0;
+        const int GREY = 1;
+        const int BLACK = 2;
+        private int n;
+        private int m;
+        private int q;
+
+
         void Solve()
         {
+            Input.Next(out n, out m, out q);
+            var W = new int[n];
+            tree = new List<int>[n];
+            colors = new byte[n];
+            back = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                back[i] = i;
+                Input.Next(out W[i]);
+                tree[i] = new List<int>();
+            }
+            
+            for (var i = 0; i < m; i++)
+            {
+                int a, b;
+                Input.Next(out a, out b);
+                a--; b--;
+                tree[a].Add(b);
+                tree[b].Add(a);
+            }
 
+            Dfs(0);
+        }
+
+        private int real(int number)
+        {
+            while (back[number] != number) number = back[number];
+            return number;
+        }
+        
+        private int Dfs(int current)
+        {
+            var vertexes = tree[current];
+            var collapse = -1;
+            foreach(var b in vertexes.ToList())
+            {
+                if (colors[b] == WHITE)
+                {
+                    collapse = Dfs(b);
+                    if (collapse == -1) // do not need to collapse
+                    {
+
+                    }
+                    else
+                    {
+                        shouldCollapseCurrentItemIntoParent = collapse == current;
+                    }
+                }
+                else
+                {
+                    back[real(current)] = b;
+                }
+            }
+            return collapse;
         }
 
         public static void Main()
