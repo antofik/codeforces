d61366ce131998ac753821b0dc7616e3fa5dff6c
Anton Naumenko <antofik@gmail.com>
2025-04-16 19:18:50 +0300

    Contest finished

--- Codeforces/TaskC/TaskC.cs
+++ Codeforces/TaskC/TaskC.cs
@@ -8,8 +8,47 @@ namespace Codeforces.Task
     {
         private void Solve(int test)
         {
-            int n = Input.Int();
-            int[] A = Input.ArrayInt();
+            Input.Next(out long n, out long m);
+            long[] A = Input.ArrayLong();
+            var counts = new long[n + m + 1];
+            var started = new long[n + m + 1];
+
+            for (int i=0;i<started.Length;++i)
+            {
+                started[i] = -1;
+            }
+
+            for (int i=1;i<=n;++i)
+            {
+                started[A[i]] = 0;
+            }
+
+            long ans = 0;
+            long prev = 0;
+            for(int j=1;j<=m;++j)
+            {
+                Input.Next(out long p, out long v);
+
+                long u = A[p];
+                A[p] = v;
+
+                if (started[u] != -1)
+                    counts[u] += j - started[u];
+                started[u] = -1;
+
+                if (started[v] != -1)
+                    counts[v] += j - started[v];
+                started[v] = j;
+
+                ans += j * n;
+                long countOld = counts[u];
+                long countNew = counts[v];
+
+                long bonus = prev - (j - countOld) + (j - countNew);
+                ans += bonus;
+                prev = bonus;
+            }
+            Console.WriteLine(ans);
         }
 
         private void Solve()
