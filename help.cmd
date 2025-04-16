d561ddf8026c47f2a41ea18ed1d5bcb511c3947e
Anton Naumenko <antofik@gmail.com>
2025-04-16 15:10:22 +0300

    new scripts

---
+++ help.cmd
@@ -0,0 +1,6 @@
+@echo off
+echo.
+echo Codeforces helper:
+echo.
+echo     new 1234 - checkout new branch '@1234' out of current (e.g. master)
+echo     finish - commit all the changes to current branch and checkout master
