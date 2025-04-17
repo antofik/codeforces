1d0bcfd4a906fc7d908adcf9dd8f9e9f567ff459
Anton Naumenko <antofik@gmail.com>
2025-04-18 00:48:54 +0300

    Fixes

--- new.cmd
+++ new.cmd
@@ -1,14 +1,15 @@
 @echo off
 
-rem git checkout -b @%1
+git checkout -b @%1
 
 call add.cmd A
-rem call add.cmd B
-rem call add.cmd C
-rem call add.cmd D
-rem call add.cmd E
-rem call add.cmd F
+call add.cmd B
+call add.cmd C
+call add.cmd D
+call add.cmd E
+call add.cmd F
 
+echo.
 echo Use next command to add new tasks:
 echo.
 echo   add G2
